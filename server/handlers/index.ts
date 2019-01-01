9a67891d4a4a8ef4caebe00c78ad3a45b34a938d
TngSam <ash_dj@mail.ru>
2019-01-01 12:09:48 +0300

    :building_construction: Extended IHandler and added verbose console logger

--- server/handlers/index.ts
+++ server/handlers/index.ts
@@ -1,13 +1,18 @@
 import handler = require('./index');
-import { Mongoose } from 'mongoose';
+import { Mongoose, Model } from 'mongoose';
 import { loggers, Logger } from 'winston';
+import { DefaultLogger } from 'utils/console';
 
 import UserHandler = require('./user/handler');
 
 export interface Handler {
   mongoose: Mongoose;
   logger: Logger;
+  verbose: DefaultLogger;
+  model: Model<any>;
   create: (data: any, verbose?: boolean) => void;
+  delete: (data: any, verbose?: boolean) => void;
+  find: (data: any, verbose?: boolean) => void;
 }
 
 export interface HandlersObject {
@@ -20,9 +25,9 @@ export interface HandlersObject {
  * @returns {{user: UserHandler}}
  * @author Samir Amirseidov <famirseidov@gmail.com>
  */
-export const invokeHandler = (mongooseInstance: Mongoose): HandlersObject => {
+export const invokeHandler = (mongooseInstance: Mongoose, console: DefaultLogger): HandlersObject => {
   const logger: Logger = loggers.get('database');
   return {
-    user: new UserHandler(mongooseInstance, logger)
+    user: new UserHandler(mongooseInstance, logger, console)
   }
 };
