b49aa49f56d41f4838f62ae9f32df8f311d00404
TngSam <ash_dj@mail.ru>
2019-01-01 12:08:08 +0300

    :sparkles: Introducing new methods - delete and find users

--- server/handlers/user/handler.ts
+++ server/handlers/user/handler.ts
@@ -1,6 +1,7 @@
-import { Mongoose, Model } from 'mongoose';
+import { Mongoose, Model, Query } from 'mongoose';
 import { Handler } from 'handlers';
 import { Logger } from 'winston';
+import { DefaultLogger, ConsoleLogger } from 'utils/console';
 import Models = require('models');
 
 /**
@@ -12,28 +13,89 @@ import Models = require('models');
 class UserHandler implements Handler {
   mongoose: Mongoose;
   logger: Logger;
-  constructor (mongooseInstance: Mongoose, logger: Logger) {
+  verbose: DefaultLogger;
+  model: Model<Models.User.Model>;
+  constructor (mongooseInstance: Mongoose, logger: Logger, verbose: DefaultLogger) {
     this.mongoose = mongooseInstance;
     this.logger = logger;
+    this.verbose = verbose;
+
+    this.model = mongooseInstance.model('User', Models.User.Schema);
   }
 
   /**
-   * Create a new user
+   * Create new user
    * @method
    * @param data - User data
    * @param verbose - Enable console logging or not
    * @author Samir Amirseidov <famirseidov@gmail.com>
    */
-  create = (data: Models.User.Interface, verbose: boolean = false): void => {
-    const User: Model = this.mongoose.model('User', Models.User.Schema);
-    new User(data).save((error: any) => {
-      if (error) return console.error(error);
+  create = async (data: Models.User.Interface, verbose: boolean = false): Promise<any> => {
+    try {
+      await new this.model(data).save();
+
+      if (verbose) {
+        this.verbose.success('New user was created.');
+      }
+
       this.logger.info('New user was created.', {
         handler: 'user',
         method: 'create',
         data
       });
-    });
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+
+  /**
+   * Delete user
+   * @param data - User data
+   * @param verbose - Enable console logging or not
+   * @author Samir Amirseidov <famirseidov@gmail.com>
+   */
+  delete = async (data: any, verbose: boolean = false): Promise<any> => {
+    const documentsCount: number = await this.model.find({ nickname: data.nickname }).estimatedDocumentCount();
+    if (documentsCount > 0) {
+      this.model.deleteOne({ nickname: data.nickname }, (error: any) => {
+        if (error) return console.error(error);
+
+        if (verbose) {
+          this.verbose.success(`User ${data.nickname} was deleted.`);
+        }
+
+        this.logger.info('User was deleted.', {
+          handler: 'user',
+          method: 'delete',
+          data
+        });
+      });
+    } else {
+      this.verbose.error('Wow! No documents here.');
+    }
+  }
+
+  /**
+   *
+   * @param data - Search criteria
+   * @param verbose - Enable console logging or not
+   * @author Samir Amirseidov <famirseidov@gmail.com>
+   */
+  find = async (data: any, verbose: boolean = false): Promise<any> => {
+    try {
+      const result = await this.model.find(data, (error: any, arr: any) => {
+        if (error) throw error;
+
+        if (verbose) {
+          this.verbose.info(`Found ${arr.length} entities by search.`);
+        }
+
+        return Promise.resolve(arr);
+      });
+      return result;
+    } catch (error) {
+      return console.error(error);
+    }
   }
 }
 
