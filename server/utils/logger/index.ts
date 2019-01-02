e7a033ca76ea3af0d80cce0b4a345059aa320885
TngSam <ash_dj@mail.ru>
2019-01-02 17:39:12 +0300

    :building_construction: Removed unnecessary interface and maked architectural changes

--- server/utils/logger/index.ts
+++ server/utils/logger/index.ts
@@ -1,25 +1,10 @@
-import { Logger, loggers } from 'winston';
-
 import initDatabaseLogger from './database';
 
 /**
- * Interface which represents an object of loggers
- * @interface
- * @author Samir Amirseidov <famirseidov@gmail.com>
- */
-export interface LoggersObject {
-  readonly [key: string]: Logger;
-}
-
-/**
  * Init loggers
- * @returns {Promise<{LoggersObject}>}
  * @author Samir Amirseidov <famirseidov@gmail.com>
  */
-export const initLoggers = async (): Promise<LoggersObject> => {
-  const DB_LOGGER_NAME = await initDatabaseLogger();
-
-  return {
-    database: loggers.get(DB_LOGGER_NAME)
-  };
+const initLoggers = async (): Promise<void> => {
+  await initDatabaseLogger();
 };
+export default initLoggers;
