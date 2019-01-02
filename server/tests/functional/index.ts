f86c11c2856a82c9b4b9b631441d959d809be2d5
TngSam <ash_dj@mail.ru>
2019-01-02 17:41:12 +0300

    :construction: Writing tests

---
+++ server/tests/functional/index.ts
@@ -0,0 +1,26 @@
+'use strict';
+const Hapi = require('hapi');
+const { expect } = require('code');
+const { test, experiment, before } = exports.lab = require('lab').script();
+import injectRouter from 'router';
+
+/**
+ * Init a Hapi server
+ * @type {Hapi.Server}
+ * @author Samir Amirseidov <famirseidov@gmail.com>
+ */
+const server = new Hapi.Server();
+
+experiment('Users handler creates new user without errors', () => {
+  before(async () => {
+    await injectRouter(server);
+  });
+  test('Returns OK when user was created', async () => {
+    const response = await server.inject({
+      method: 'POST',
+      url: '/user/create?nickname=asdqwe&password=qwelols'
+    });
+
+    expect(response.statusCode).to.equal(200);
+  });
+});
