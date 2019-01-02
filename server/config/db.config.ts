73257a833f8b5e24634d0dfb42d773b738c306a2
TngSam <ash_dj@mail.ru>
2019-01-02 17:35:51 +0300

    :recycle: Changed modules export view

--- server/config/db.config.ts
+++ server/config/db.config.ts
@@ -1,11 +1,6 @@
-/// <reference path="db.config.d.ts"/>
-
-const credentials: database.Credentials = {
+export const credentials = {
   dbUser: 'tester',
   dbPassword: 'qwerty1'
 };
 
-export default {
-  credentials,
-  url: `mongodb://${credentials.dbUser}:${credentials.dbPassword}@ds159073.mlab.com:59073/monolink-project-test`
-}
+export const url: string = `mongodb://${credentials.dbUser}:${credentials.dbPassword}@ds159073.mlab.com:59073/monolink-project-test`;
