3c4541d8274a6db359f2e694ab651a7490f92d75
TngSam <ash_dj@mail.ru>
2019-01-04 23:49:05 +0300

    :sparkles: New route '/user/delete'. Returns handlers object if NODE_ENV is 'test'

--- server/router/index.ts
+++ server/router/index.ts
@@ -10,12 +10,12 @@ import { User } from 'models';
  * @param handlers {HandlersObject} - Handlers object
  * @author Samir Amirseidov <famirseidov@gmail.com>
  */
-const injectRouter = async (server: Server, handlers: HandlersObject): Promise<void> => {
+const injectRouter = async (server: Server, handlers: HandlersObject): Promise<void | HandlersObject> => {
   /**
    * Create user route
    * @author Samir Amirseidov <famirseidov@gmail.com>
    */
-  server.route({
+  await server.route({
     method: 'POST',
     path: '/user/create',
     async handler (req: Request, h: ResponseToolkit) {
@@ -35,5 +35,36 @@ const injectRouter = async (server: Server, handlers: HandlersObject): Promise<v
       }
     }
   });
+
+  /**
+   * Delete user route
+   * @author Samir Amirseidov <famirseidov@gmail.com>
+   */
+  await server.route({
+    method: 'POST',
+    path: '/user/delete',
+    async handler (req: Request, h: ResponseToolkit) {
+      try {
+        await handlers.user.delete(req.query, true);
+        return h.response({
+          text: 'OK',
+          code: 200
+        }).code(200);
+      } catch (error) {
+        return boom.badImplementation(error);
+      }
+    },
+    options: {
+      validate: {
+        query: {
+          nickname: User.Joi.nickname
+        }
+      }
+    }
+  });
+
+  if (process.env.NODE_ENV === 'test') {
+    return handlers;
+  }
 }
 export default injectRouter;
