7c5b59cb82e302e3e22999c9f759f6826002fc81
TngSam <ash_dj@mail.ru>
2019-01-04 23:52:10 +0300

    :white_check_mark: Written tests :building_construction: Collection is clearing before every test and after all tests

--- server/tests/functional/index.ts
+++ server/tests/functional/index.ts
@@ -1,8 +1,9 @@
 'use strict';
 const Hapi = require('hapi');
 const { expect } = require('code');
-const { test, experiment, before } = exports.lab = require('lab').script();
+const { test, experiment, before, beforeEach, after } = exports.lab = require('lab').script();
 
+import buildURI from 'utils/buildUri';
 import setup from 'setup';
 
 /**
@@ -12,16 +13,52 @@ import setup from 'setup';
  */
 const server = new Hapi.Server();
 
-experiment('Users handler creates new user without errors', () => {
-  before(async () => {
-    await setup(server);
+experiment('Users handler', () => {
+  before(async ({ context }): Promise<void> => {
+    context.handlers = await setup(server);
   });
-  test('Returns OK when user was created', async () => {
+  beforeEach(async ({ context }): Promise<void> => {
+    await context.handlers.user.clear(true);
+  });
+
+  test('Returns OK on user creation', async () => {
+    const userdata = {
+      nickname: 'asdqwe',
+      password: 'qwelols'
+    };
+
     const response = await server.inject({
       method: 'POST',
-      url: '/user/create?nickname=asdqwe&password=qwelols'
+      url: buildURI('/user/create', userdata)
     });
 
     expect(response.statusCode).to.equal(200);
+  });
+  test('Finds users correctly', async ({ context }): Promise<void> => {
+    const userdata = {
+      nickname: 'monolinks',
+      password: 'qwes'
+    };
+
+    await context.handlers.user.create(userdata, true);
+
+    const result = await context.handlers.user.find({ nickname: userdata.nickname }, true);
+    expect(result.length).to.equal(1);
+  });
+  test('Deletes user successfully', async ({ context }): Promise<void> => {
+    const userdata = {
+      nickname: 'monolink',
+      password: 'qwes'
+    };
+
+    await context.handlers.user.create(userdata, true);
+    await context.handlers.user.delete(userdata, true);
+
+    const result = await context.handlers.user.find({ nickname: userdata.nickname });
+    expect(result.length).to.equal(0);
+  });
+
+  after(async ({ context }): Promise<void> => {
+    await context.handlers.user.clear(true);
   });
 });
