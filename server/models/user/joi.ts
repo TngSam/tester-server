5083b5aaa033d409271c67541ae70d23848a90eb
TngSam <ash_dj@mail.ru>
2019-02-03 21:58:44 +0300

    :memo: Added commentary

--- server/models/user/joi.ts
+++ server/models/user/joi.ts
@@ -1,5 +1,9 @@
 import Joi = require('joi');
 
+/**
+ * Joi validation for user data
+ * @author Samir Amirseidov <famirseidov@gmail.com>
+ */
 export = {
   nickname: Joi.string().alphanum().min(2).max(18).required(),
   password: Joi.string().regex(/^[a-zA-Z0-9-_]{6,30}$/).required()
