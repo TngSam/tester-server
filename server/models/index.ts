91011feebb211974750de557600d4dc44607ab10
TngSam <ash_dj@mail.ru>
2019-02-03 21:57:46 +0300

    :label: Added Test namespace

--- server/models/index.ts
+++ server/models/index.ts
@@ -2,11 +2,21 @@ import UserSchema = require('./user/schema');
 import UserJoi = require('./user/joi');
 import UserModel = require('./user/model');
 
+import TestSchema = require('./test/schema');
+import TestJoi = require('./test/joi');
+import TestModel = require('./test/model');
+
 namespace Models {
   export namespace User {
     export const Schema = UserSchema;
     export const Joi = UserJoi;
     export interface Model extends UserModel {}
+  }
+
+  export namespace Test {
+    export const Schema = TestSchema;
+    export const Joi = TestJoi;
+    export interface Model extends TestModel {}
   }
 }
 
